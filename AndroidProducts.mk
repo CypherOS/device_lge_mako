bf8aeb44e014522c6152ee2902a0b263eab05db6
nitin1438 <nitin.chobhe@gmail.com>
2016-04-13 21:20:00 +0530

    Author: Nitin1438 	Change: Synced with CM

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,4 +16,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_mako.mk \
+    $(LOCAL_DIR)/xosp_mako.mk \
     $(LOCAL_DIR)/full_mako.mk
--- xosp.mk
+++ xosp.mk
@@ -3,20 +3,22 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/xosp/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := xosp_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
+RELEASE_TYPE := XOSP_OFFICIAL
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
