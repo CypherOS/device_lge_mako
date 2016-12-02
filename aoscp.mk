954ff9833b8cf416de5257b701a9e4a0801fe1a0
nitin1438 <nitin.chobhe@gmail.com>
2016-12-02 19:43:26 +0530

    mako get's taste of aoscp :)

---
+++ aoscp.mk
@@ -0,0 +1,22 @@
+$(call inherit-product, vendor/aoscp/configs/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aoscp/configs/telephony.mk)
+
+# Inherit AOSP device configuration for mako
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Setup device specific product configuration
+PRODUCT_NAME := aoscp_mako
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := mako
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys"
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
